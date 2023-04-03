ff07abbabffca01345d0dd93cac2c36f0a3b6f06
Kuba Jasko <kubajasko@hotmail.co.uk>
2023-04-03 16:06:02 +0100

    Got the dockerhub tag checks to be dynamic

--- scripts/setupTags.pl
+++ scripts/setupTags.pl
@@ -76,23 +76,8 @@ if($opts{"show-builds"}){
 prepareRepo();
 logg({format=>0}, "Downloading versions...");
 # TODO Do all curls in parallel as it saves time
-my @dockerhub_tags = ();
 my $page = 0;
-while(1){
-    logg({format=>0}, ".");
-    $page += 1;
-    my $result = $JSON->decode(join('', @{executeOrDie("curl --silent -f -lSL 'https://hub.docker.com/v2/namespaces/bauk/repositories/git/tags?page_size=100&page=$page'")->{log}}));
-    my @tags = map { $_->{name} }  @{$result->{results}};
-    push @dockerhub_tags, @tags;
-    if($page > 30 or not @tags or not $result->{next}){
-        # We don't scrape them all
-        # TODO: Migrate this to checking individual tags when needed
-        last;
-    }
-}
-logg(0, "", "Found ".scalar(@dockerhub_tags)." tags in dockerhub");
 my @git_versions = @{executeOrDie('curl -sS https://mirrors.edge.kernel.org/pub/software/scm/git/|sed -n "s#.*git-\([0-9\.]\+\).tar.gz.*#\1#p"|sort -V')->{log}};
-logg(3, @dockerhub_tags);
 logg(2, @git_versions);
 my $latest_version = $git_versions[-1];
 if(@ARGV){
@@ -104,7 +89,6 @@ if(@ARGV){
     @git_versions = @ARGV;
 }
 if($opts{reverse}){
-    @dockerhub_tags = reverse @dockerhub_tags;
     @git_versions   = reverse @git_versions;
 }
 logg(0, "Latest version: $latest_version. Total versions: ".($#git_versions+1));
@@ -290,7 +274,7 @@ sub doDir {
                     $docker_tag = "${DEFAULT_OS}-$dir-${version}-${parent_commit}";
                 }
                 if($opts{"update-unbuilt"}
-                  && ! ((grep /^${docker_tag}$/, @dockerhub_tags)
+                  && ! ((execute("curl --silent -f -lSL 'https://hub.docker.com/v2/namespaces/bauk/repositories/git/tags/$docker_tag'")->{exit} == 0)
                      || (grep /^${DEFAULT_OS}-${version}$/, @CURRENT_BUILDS) # As builds in progress will not have the full docker tag with ID
                   )){
                     loggBufferAppend("RETAGGING TO REBUILD");
